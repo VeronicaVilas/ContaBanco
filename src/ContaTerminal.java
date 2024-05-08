4b653486d4c55356383adba613d27957ec22c4ea
Verônica Vilas Boas <135287830+VeronicaVilas@users.noreply.github.com>
2024-05-07 22:26:04 -0300

    feat: Adiciona classe scanner

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,10 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
 
-        int numero = 1021;
-        String agencia = "067-8";
-        String nome = "MARIO ANDRADE";
-        Double saldo = 237.48;
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.nextLine();
+
+        System.out.println("Por favor, agora digite o número da Agência!");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o número da sua conta");
+        int numero = scanner.nextInt();
+        
+        System.out.println("Qual valor vocÊ deseja depositar?");
+        Double saldo = scanner.nextDouble();
 
         System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
