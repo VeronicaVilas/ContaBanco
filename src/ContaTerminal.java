e04b5f5a14dc2b5a2c9fc3de404d5f51a4c843ef
Verônica Vilas Boas <135287830+VeronicaVilas@users.noreply.github.com>
2024-05-07 22:14:59 -0300

    feat: Adiciona variáveis

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,13 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
 
+        int numero = 1021;
+        String agencia = "067-8";
+        String nome = "MARIO ANDRADE";
+        Double saldo = 237.48;
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
+
+
